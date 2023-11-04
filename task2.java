57104665d0ac0194b338352f3e94a3000d70822d
cynthia43 <149767375+cynthia43@users.noreply.github.com>
2023-11-04 03:29:12 -0800

    Update task2.java

--- task2.java
+++ task2.java
@@ -1,45 +1,25 @@
 import java.util.Scanner;
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
 
-public class task2 {
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan tanggal lahir Kamnyuu (Contoh 29 04): ");
-        String tanggalLahir = input.nextLine();
+        System.out.print("Masukkan sebuah bilangan bulat:");
+        number = scanner.nextInt();
 
-        String[] parts = tanggalLahir.split(" ");
-        int tanggal = Integer.parseInt(parts[0]);
-        int bulan = Integer.parseInt(parts[1]);
+        while (number != 0) {
+            if (number > 0) {
+                System.out.println("Bilangan positif.");
+            } else {
+                System.out.println("Bilangan negatif.");
+            }
 
-        String zodiak = hitungZodiak(tanggal, bulan);
-        System.out.println("Zodiak Anda adalah: " + zodiak);
-    }
-
-    public static String hitungZodiak(int tanggal, int bulan) {
-        if ((bulan == 3 && tanggal >= 21) || (bulan == 4 && tanggal <= 19)) {
-            return "Aries";
-        } else if ((bulan == 4 && tanggal >= 20) || (bulan == 5 && tanggal <= 20)) {
-            return "Taurus";
-        } else if ((bulan == 5 && tanggal >= 21) || (bulan == 6 && tanggal <= 20)) {
-            return "Gemini";
-        } else if ((bulan == 6 && tanggal >= 21) || (bulan == 7 && tanggal <= 22)) {
-            return "Cancer";
-        } else if ((bulan == 7 && tanggal >= 23) || (bulan == 8 && tanggal <= 22)) {
-            return "Leo";
-        } else if ((bulan == 8 && tanggal >= 23) || (bulan == 9 && tanggal <= 22)) {
-            return "Virgo";
-        } else if ((bulan == 9 && tanggal >= 23) || (bulan == 10 && tanggal <= 22)) {
-            return "Libra";
-        } else if ((bulan == 10 && tanggal >= 23) || (bulan == 11 && tanggal <= 21)) {
-            return "Scorpio";
-        } else if ((bulan == 11 && tanggal >= 22) || (bulan == 12 && tanggal <= 21)) {
-            return "Sagittarius";
-        } else if ((bulan == 12 && tanggal >= 22) || (bulan == 1 && tanggal <= 19)) {
-            return "Capricorn";
-        } else if ((bulan == 1 && tanggal >= 20) || (bulan == 2 && tanggal <= 18)) {
-            return "Aquarius";
-        } else {
-            return "Pisces";
+            System.out.print("Masukkan sebuah bilangan bulat (atau 0 untuk keluar): ");
+            number = scanner.nextInt();
         }
+
+        System.out.println("Program selesai.");
+        scanner.close();
     }
 }
 
